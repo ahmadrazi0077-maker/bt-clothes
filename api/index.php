475a478137a3f45a1effd8d05ea20849e17a25c0
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-08-10 08:04:56 +0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,3 @@
 <?php
 
-// ✅ Check if vendor exists
-if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    die('vendor/autoload.php not found. Run composer install.');
-}
-
-// Forward Vercel requests to public/index.php
-require __DIR__ . '/../public/index.php';
+echo 'PHP Vercel is working';
