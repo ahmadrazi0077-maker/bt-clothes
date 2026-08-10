0a10b5453fe627f32961f656dd9de79367fdd960
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-08-10 23:37:01 +0500

    500neroror

--- public/index.php
+++ public/index.php
@@ -4,18 +4,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// ✅ Check vendor
-if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
-    die('vendor/autoload.php not found');
-}
-
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
-
-// ✅ Check bootstrap/app.php
-if (!file_exists(__DIR__.'/../bootstrap/app.php')) {
-    die('bootstrap/app.php not found');
-}
 
 // Bootstrap Laravel and handle the request...
 $app = require_once __DIR__.'/../bootstrap/app.php';
