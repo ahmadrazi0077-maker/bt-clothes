d788a5ee35ad80b2c3a1b977f3bcf0c2ae6b3521
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-08-10 14:45:32 +0500

    Fix Laravel Vercel deployment

--- api/index.php
+++ api/index.php
@@ -3,30 +3,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-echo "STEP 1<br>";
-
 require __DIR__ . '/../vendor/autoload.php';
-
-echo "STEP 2<br>";
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-echo "STEP 3<br>";
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-echo "STEP 4<br>";
-
 $request = Illuminate\Http\Request::capture();
 
-echo "STEP 5<br>";
-
-$response = $kernel->handle($request);
-
-echo "STEP 6<br>";
+$response = $app->handleRequest($request);
 
 $response->send();
-
-echo "STEP 7<br>";
-
-$kernel->terminate($request, $response);
